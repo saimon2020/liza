dc7fb6325ee9a957e1713366c9857b232a7cee56
Sam Fedorov <semenfx@yandex.ru>
2023-01-29 22:45:57 +0300

    Remove extra modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,11 +7,3 @@ pluginManagement {
         kotlin("jvm") version kotlinVersion
     }
 }
-
-
-include("m1l1-quickstart")
-include("m1l3-oop")
-include("m1l4-dsl")
-include("m1l5-coroutines")
-include("m1l6-flows-and-channels")
-include("m1l7-kmp")
